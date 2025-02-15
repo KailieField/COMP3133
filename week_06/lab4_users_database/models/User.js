1c692a07b8a722c63a14bc0263af9b9ac9635c3b
Kailie Field <kailie.field@gmail.com>
2025-02-15 18:29:02 -0500

    UPDATING: schema to match .JSON data provided..

--- week_06/lab4_users_database/models/User.js
+++ week_06/lab4_users_database/models/User.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
 
+    name: {
+
+        type: String,
+        required: true
+
+    },
+
     username: {
         
         type: String,
@@ -19,14 +26,64 @@ const UserSchema = new mongoose.Schema({
 
     },
 
-    city: {
+    address: {
 
-        type: String,
-        required: true,
-        match: [/^[A-Za-z\s]+$/, "City can only have alphabet letters and spaces when entered."],
+        street: {
+
+            type: String,
+            required: true
+
+        },
+
+        suit: {
+
+            type: String,
+            required: true
+
+        },
+
+        city: {
+
+            type: String,
+            required: true,
+            match: [/^[A-Za-z\s]+$/, "City can only have alphabet letters and spaces when entered."],
+
+        },
+
+        zip_code: {
+
+            type: String,
+            required: true,
+            match: [ /^\d{5}-\d{4}$/, "Zip Code must be formatted as 12345-1234"],
+    
+        },
+
+        geo: {
+
+            lat: {
+
+                type: String,
+                required: true
+
+            },
+
+            lng: {
+
+                type: String,
+                required: true
+
+            },
+
+        },
+
+    phone: {
+
+            type: String,
+            required: true,
+            match: [ /^1-\d{3}-\d{3}-\{4}$/, "Phone Number must be entered as: 1-123-123-1234" ],
 
     },
-
+    
     website: {
 
         type: String,
@@ -35,13 +92,32 @@ const UserSchema = new mongoose.Schema({
 
     },
 
-    zip_code: {
+    company: {
 
-        type: String,
-        required: true,
-        match: [ /^1-\d{3}-\d{3}-\{4}$/, "Phone Number must be entered as: 1-123-123-1234" ],
+        name: {
+
+            type: String,
+            required: true,
+
+        },
+
+        catchPhrase: {
+
+            type: String,
+            required: true, 
+
+        },
+
+        bs: {
+
+            type: String,
+            required: true
+
+        },
 
     },
+}
+
 
 
 });
