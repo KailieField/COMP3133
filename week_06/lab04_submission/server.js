ece3619134785480de6fd12993575ec58bd8ac14
Kailie Field <kailie.field@gmail.com>
2025-02-15 22:34:30 -0500

    redoing entire lab.

---
+++ week_06/lab04_submission/server.js
@@ -0,0 +1,39 @@
+require('dontenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyparser = require('body-parser')
+const User = require('./models/User')
+
+const app = express()
+const PORT = process.env.PORT || 8081
+
+app.use(bodyparser.json())
+
+
+mongoose.connect(DB_CONNECT).then(() => {
+
+    console.log(`--- CONNECTED TO MONGODB ---`)
+
+})
+.catch((error) => {
+
+    console.error('--- FAILURE CONNECTING TO MONGODV: ', error.message)
+
+})
+
+app.post('/users', async (req, res) => {
+
+    try {
+
+        const user =new User(req.body);
+        await user.save()
+        res.status(201).json({ message: 'USER ADDED TO DATABASE', user })
+
+    } catch (error) {
+
+        res.status(400).json({ error: error.message })
+        
+    }
+});
+
+app.listen(PORT, () => console.log(` SERVER RUNNIG ON ${PORT}: http://localhost:8081` ));
