73900e29ae73a20d5bbad8a6859157779d90eccb
Kailie Field <kailie.field@gmail.com>
2025-02-16 00:50:01 -0500

    baseline Employee schema added

---
+++ COMP3133_100627702_Assignment1/models/Employee.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const EmployeeSchema = new mongoose.Schema({
+
+    first_name: {
+        type: String, 
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ["Male", "Female", "Other"]
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    salary: {
+        type: Number,
+        required: true
+    },
+    date_of_joining: {
+        type: Date,
+        default: Date.now()
+    },
+    department: {
+        type: String,
+        required: true,
+    },
+    employee_photo: {
+        type: String
+    },
+    created_at: {
+        type: Date,
+        default: Date.now()
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now()
+    },
+
+});
+
+module.exports = new mongoose.model("Employee", EmployeeSchema);
