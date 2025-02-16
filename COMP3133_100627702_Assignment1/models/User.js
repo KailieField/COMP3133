0b2552c8ba27fcebe303760b5adeccf4397fc95a
Kailie Field <kailie.field@gmail.com>
2025-02-16 00:43:00 -0500

    baseline schema for User

---
+++ COMP3133_100627702_Assignment1/models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+
+    username: {
+
+        type: String,
+        required: true,
+        unique: true,
+
+    },
+
+    email: {
+
+        type: String,
+        required: true,
+        unique: true
+
+    },
+
+    password: {
+
+        type: String,
+        required: true
+
+    },
+
+    created_at: {
+
+        type: Date,
+        default: Date.now()
+
+    },
+
+    updated_at: {
+
+        type: Date,
+        default: Date.now()
+
+    },
+
+});
+
+module.exports = new mongoose.model('User', UserSchema);
