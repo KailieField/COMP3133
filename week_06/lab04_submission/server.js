dbb2477c938d12412796fbb7c3aa3fd2408221ab
Kailie Field <kailie.field@gmail.com>
2025-02-15 23:11:18 -0500

    this is absolutely ridiculous

--- week_06/lab04_submission/server.js
+++ week_06/lab04_submission/server.js
@@ -1,40 +1,7 @@
 require('dotenv').config()
+const fs = require('fs')
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyparser = require('body-parser')
 const User = require('./models/User')
 
-const app = express()
-
-app.use(bodyparser.json())
-
-const PORT = process.env.PORT || 8081
-const DB_CONNECT = process.env.DB_CONNECT || 8081
-
-mongoose.connect(DB_CONNECT).then(() => {
-
-    console.log(`--- CONNECTED TO MONGODB ---`)
-
-})
-.catch((error) => {
-
-    console.error('--- FAILURE CONNECTING TO MONGODV: ', error.message)
-
-})
-
-app.post('/users', async (req, res) => {
-
-    try {
-
-        const user =new User(req.body);
-        await user.save()
-        res.status(201).json({ message: 'USER ADDED TO DATABASE', user })
-
-    } catch (error) {
-
-        res.status(400).json({ error: error.message })
-
-    }
-});
-
-app.listen(PORT, () => console.log(` SERVER RUNNIG ON ${PORT}: http://localhost:8081` ));
