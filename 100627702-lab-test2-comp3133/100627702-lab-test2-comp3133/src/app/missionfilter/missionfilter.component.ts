09898413cfbd0b0ab4443c8a86fa8cd61491b54c
Kailie Field <kailie.field@gmail.com>
2025-04-02 15:12:55 -0400

    filter is functioning within development console, connecting app.component.html to missionfilter.component

--- 100627702-lab-test2-comp3133/100627702-lab-test2-comp3133/src/app/missionfilter/missionfilter.component.ts
+++ 100627702-lab-test2-comp3133/100627702-lab-test2-comp3133/src/app/missionfilter/missionfilter.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-missionfilter',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './missionfilter.component.html',
-  styleUrl: './missionfilter.component.css'
+  styleUrls: ['./missionfilter.component.css']
 })
+
 export class MissionfilterComponent {
+  
+  year: string='';
+
+  filterMissionList(){
+
+    console.log('FILTER -- (YEAR): ', this.year);
+    
+  }
 
 }
