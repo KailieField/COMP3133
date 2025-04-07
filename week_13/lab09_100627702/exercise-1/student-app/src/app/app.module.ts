168f45491eec71767b50485b26714144eff0aadf
Kailie Field <kailie.field@gmail.com>
2025-04-06 22:35:05 -0400

    updating app.module.ts

--- week_13/lab09_100627702/exercise-1/student-app/src/app/app.module.ts
+++ week_13/lab09_100627702/exercise-1/student-app/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+
+    AppComponent,
+    StudentsComponent
+
   ],
+
   imports: [
+
     BrowserModule,
     AppRoutingModule
+
   ],
+
   providers: [],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
