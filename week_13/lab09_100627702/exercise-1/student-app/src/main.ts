97eadf19f63e1cce1c704c0b726aac11b7a73d06
Kailie Field <kailie.field@gmail.com>
2025-04-06 23:01:46 -0400

    this is....i dont know whats going on. researching

--- week_13/lab09_100627702/exercise-1/student-app/src/main.ts
+++ week_13/lab09_100627702/exercise-1/student-app/src/main.ts
@@ -1,7 +1,6 @@
-import { platformBrowser } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { isPlatformBrowser } from '@angular/common';
 
-platformBrowser().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
-})
-  .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
+.catch(err => console.log(err));
